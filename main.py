7f19367eaeb9243a54e45cf300ceb3db37e1284a
Batman <anuragsing092@gmail.com>
2025-02-08 23:15:44 +0545

    loop and if-else conditions

--- main.py
+++ main.py
@@ -16,15 +16,30 @@ def view_task():
 
 def main():
 
-    print("*****************************")
-    print(" **Welcome to** \nTo-Do List Manager")
-    print("1. Add a Task")
-    print("2. Remove a Task")
-    print("3. Mark a Task as Completed")
-    print("4. View Tasks")
-    print("5. Exit")
+    while True:
+        print("*****************************")
+        print(" **Welcome to** \nTo-Do List Manager")
+        print("1. Add a Task")
+        print("2. Remove a Task")
+        print("3. Mark a Task as Completed")
+        print("4. View Tasks")
+        print("5. Exit")
 
-    choice = input("Enter your choice (1-5): ")
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            remove_task()
+        elif choice == "3":
+            mark_task_complete()
+        elif choice == "4":
+            view_task()
+        elif choice == "5":
+            print("Have a nice day! Bye..")
+            break
+        else:
+            print("Invalid Choice.\n Try again........")
 
 if __name__ == "__main__":
     main()
