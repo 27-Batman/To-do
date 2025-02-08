3d6c616afdd5dea4e0f0104907ff453b38e81581
Batman <anuragsing092@gmail.com>
2025-02-08 23:11:12 +0545

    functions

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 # Python To do list
 
-print("*****************************")
-print(" **Welcome to** \nTo-Do List Manager")
-print("1. Add a Task")
-print("2. Remove a Task")
-print("3. Mark a Task as Completed")
-print("4. View Tasks")
-print("5. Exit")
+task = []
 
-choice = input("Enter your choice (1-5): ")
+def add_task():
+    pass
+
+def remove_task():
+    pass
+
+def mark_task_complete():
+    pass
+
+def view_task():
+    pass
+
+def main():
+
+    print("*****************************")
+    print(" **Welcome to** \nTo-Do List Manager")
+    print("1. Add a Task")
+    print("2. Remove a Task")
+    print("3. Mark a Task as Completed")
+    print("4. View Tasks")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+if __name__ == "__main__":
+    main()
