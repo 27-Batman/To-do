63c9b6e60811f22887ac76636dbcfe764778719a
Batman <anuragsing092@gmail.com>
2025-02-08 23:08:13 +0545

    Options

---
+++ main.py
@@ -0,0 +1,11 @@
+# Python To do list
+
+print("*****************************")
+print(" **Welcome to** \nTo-Do List Manager")
+print("1. Add a Task")
+print("2. Remove a Task")
+print("3. Mark a Task as Completed")
+print("4. View Tasks")
+print("5. Exit")
+
+choice = input("Enter your choice (1-5): ")
